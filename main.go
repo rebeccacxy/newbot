6672a252075894b224df5e2c309b98ec3bfe73d7
rebecca <rebeccachinxy@gmail.com>
2020-05-02 21:42:57 +0800

    Update main.go

--- main.go
+++ main.go
@@ -8,13 +8,13 @@ import (
 	"github.com/yanzay/tbot/v2"
 )
 
-type score struct {
-	wins, draws, losses uint
-}
+// type score struct {
+// 	wins, draws, losses uint
+// }
 
 type application struct {
 	client *tbot.Client
-	score
+	//	score
 }
 
 var (
@@ -38,12 +38,23 @@ func init() {
 }
 
 func main() {
-	bot = tbot.New(token, tbot.WithWebhook("https://rochambeau-bot.herokuapp.com", ":"+os.Getenv("PORT")))
+	// bot = tbot.New(token, tbot.WithWebhook("https://rochambeau-bot.herokuapp.com", ":"+os.Getenv("PORT")))
+	// app.client = bot.Client()
+	// bot.HandleMessage("/start", app.startHandler)
+	// bot.HandleMessage("/play", app.playHandler)
+	// bot.HandleMessage("/score", app.scoreHandler)
+	// bot.HandleMessage("/reset", app.resetHandler)
+	// bot.HandleCallback(app.callbackHandler)
+	// log.Fatal(bot.Start())
+	bot = tbot.New(token)
 	app.client = bot.Client()
 	bot.HandleMessage("/start", app.startHandler)
 	bot.HandleMessage("/play", app.playHandler)
-	bot.HandleMessage("/score", app.scoreHandler)
-	bot.HandleMessage("/reset", app.resetHandler)
 	bot.HandleCallback(app.callbackHandler)
 	log.Fatal(bot.Start())
 }
+
+// func (a *application) startHandler(m *tbot.Message) {
+// 	msg := "This bot will play rock paper scissors with you."
+// 	a.client.SendMessage(m.Chat.ID, msg)
+// }
