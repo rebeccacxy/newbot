401b71bcfd5054b5020773afae70634d773617eb
rebecca <rebeccachinxy@gmail.com>
2020-05-02 21:42:38 +0800

    Update handlers.go

--- handlers.go
+++ handlers.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"fmt"
+	//	"fmt"
 
 	"github.com/yanzay/tbot/v2"
 )
 
 // Handle the /start command here
 func (a *application) startHandler(m *tbot.Message) {
-	msg := "This is a bot whose sole purpose is to play rock, paper, scissors with you.\nCommands:\n1. Use /play to play.\n2. Use /score to view current scores.\n3. Use /reset to reset scores."
+	msg := "This is a bot that will play rock papers scissors. YES SIS"
 	a.client.SendMessage(m.Chat.ID, msg)
 }
 
@@ -19,22 +19,22 @@ func (a *application) playHandler(m *tbot.Message) {
 }
 
 // Handle the /score command here
-func (a *application) scoreHandler(m *tbot.Message) {
-	msg := fmt.Sprintf("Scores:\nWins: %v\nDraws: %v\nLosses: %v", a.wins, a.draws, a.losses)
-	a.client.SendMessage(m.Chat.ID, msg)
-}
+// func (a *application) scoreHandler(m *tbot.Message) {
+// 	msg := fmt.Sprintf("Scores:\nWins: %v\nDraws: %v\nLosses: %v", a.wins, a.draws, a.losses)
+// 	a.client.SendMessage(m.Chat.ID, msg)
+// }
 
 // Handle the /reset command here
-func (a *application) resetHandler(m *tbot.Message) {
-	a.wins, a.draws, a.losses = 0, 0, 0
-	msg := "Scores have been reset to 0."
-	a.client.SendMessage(m.Chat.ID, msg)
-}
+// func (a *application) resetHandler(m *tbot.Message) {
+// 	a.wins, a.draws, a.losses = 0, 0, 0
+// 	msg := "Scores have been reset to 0."
+// 	a.client.SendMessage(m.Chat.ID, msg)
+// }
 
 // Handle buttton presses here
 func (a *application) callbackHandler(cq *tbot.CallbackQuery) {
 	humanMove := cq.Data
-	msg := a.draw(humanMove)
+	msg := draw(humanMove)
 	a.client.DeleteMessage(cq.Message.Chat.ID, cq.Message.MessageID)
 	a.client.SendMessage(cq.Message.Chat.ID, msg)
 }
